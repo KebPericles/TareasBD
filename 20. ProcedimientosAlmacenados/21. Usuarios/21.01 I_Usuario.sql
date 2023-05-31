51a4956fc7dd454c30726f47b6e763ef970d22e3
Luis Guzman <guzman.barriga.luis@gmail.com>
2023-05-31 17:44:29 -0600

    Correccion de nombre y selccion de tabla

--- 20. ProcedimientosAlmacenados/21. Usuarios/21.01 I_Usuario.sql
+++ 20. ProcedimientosAlmacenados/21. Usuarios/21.01 I_Usuario.sql
@@ -2,7 +2,7 @@
 --Nuevo usuario
 
 DELIMITER //
-CREATE PROCEDURE I_Usuarios
+CREATE PROCEDURE I_Usuario
 	(
     in p_correo VARCHAR(45),
 	p_contrasena VARCHAR(45),
@@ -10,7 +10,7 @@ CREATE PROCEDURE I_Usuarios
 	p_apellido VARCHAR(45)
     )
 BEGIN
-	IF NOT EX  ISTS(select *from usuario where correo = p_correo)
+	IF NOT EXISTS(select *from usuarios where correo = p_correo)
 		then INSERT INTO usuarios value(default,p_correo,p_contrasena,p_nombre,p_apellido);
 	END IF;
 END //
