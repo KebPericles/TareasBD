ab7f3fbb518160df171127976dc4baad33742641
Luis Guzman <guzman.barriga.luis@gmail.com>
2023-06-02 16:24:04 -0600

    Ajuste de update y eliminar u_subtareas

--- 20. ProcedimientosAlmacenados/23. Tareas/23.07 U_Tarea.sql
+++ 20. ProcedimientosAlmacenados/23. Tareas/23.07 U_Tarea.sql
@@ -2,10 +2,11 @@
 -- Actualizar datos de una tarea
 
 DROP PROCEDURE IF EXISTS U_Tarea;
+
 DELIMITER //
 CREATE PROCEDURE U_Tarea(
 	in p_idTarea int,
-    in p_titulo varchar(45),
+    in p_titulo varchar(100),
     in p_descripcion varchar(200),
     in p_fechaInicio datetime,
     in p_fechaVencimiento datetime,
@@ -14,34 +15,33 @@ CREATE PROCEDURE U_Tarea(
 )
 BEGIN
     -- Verificar que el ID de tarea existe en la tabla de Tareas
-    IF NOT EXISTS (SELECT * FROM Tareas WHERE idTarea = p_idTarea)
+    IF NOT EXISTS (SELECT * FROM tareas WHERE idTarea = p_idTarea)
         THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El ID de tarea no existe.';
     ELSE
-        -- Si hay valore nulos
-        IF(p_titulo is NULL)
-            THEN set p_titulo = (select titulo from tarea where idTarea = p_idTarea );
-        END IF;
-        IF(p_descripcion is NULL)
-            THEN set p_descripcion = (select descripcion from tarea where idTarea = p_idTarea );
-        END IF;
-        IF(p_fechaInicio is NULL)
-            THEN set p_fechaInicio = (select fechaInicio from tarea where idTarea = p_idTarea );
-        END IF;
-        IF(p_fechaVencimiento is NULL)
-            THEN set p_fechaVencimiento = (select fechaVencimiento from tarea where idTarea = p_idTarea );
-        END IF;
-        IF(p_prioridad is NULL)
-            THEN set p_prioridad = (select prioridad from tarea where idTarea = p_idTarea );
-        END IF;
-        IF(p_estado is NULL)
-            THEN set p_estado = (select estado from tarea where idTarea = p_idTarea );
-        END IF;
         -- Si existe con mismo nombre
         IF EXISTS(select *from tareas where titulo = p_titulo) 
             THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ya existe una carpeta con el mismo titulo y misma posicion';
-        ELSEIF(p_fechaInicio < now())
-            THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La fecha de inicio es menor a la fecha en la que se creo la tarea';
-        ELSEIF(p_fechaInicio > p_fechaVencimiento)
+		END IF;
+            -- Si hay valore nulos
+        IF(p_titulo is NULL)
+            THEN set p_titulo = (select titulo from tareas where idTarea = p_idTarea );
+        END IF;
+        IF(p_descripcion is NULL)
+            THEN set p_descripcion = (select descripcion from tareas where idTarea = p_idTarea );
+        END IF;
+        IF(p_fechaInicio is NULL)
+            THEN set p_fechaInicio = (select fechaInicio from tareas where idTarea = p_idTarea );
+        END IF;
+        IF(p_fechaVencimiento is NULL)
+            THEN set p_fechaVencimiento = (select fechaVencimiento from tareas where idTarea = p_idTarea );
+        END IF;
+        IF(p_prioridad is NULL)
+            THEN set p_prioridad = (select prioridad from tareas where idTarea = p_idTarea );
+        END IF;
+        IF(p_estado is NULL)
+            THEN set p_estado = (select estado from tareas where idTarea = p_idTarea );
+        END IF;
+        IF(p_fechaInicio > p_fechaVencimiento)
             THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La fecha de vencimiento es menor a la fecha de inicio';
         ELSE
             -- Actualizar el nombre de la subcarpeta
