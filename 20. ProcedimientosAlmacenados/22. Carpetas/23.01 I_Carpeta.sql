78eaacd7875603376f7e5d471fe955be5e757864
KebPericles <45951033+KebPericles@users.noreply.github.com>
2023-06-01 19:40:07 -0600

    Update 23.01 I_Carpeta.sql Formato

--- 20. ProcedimientosAlmacenados/22. Carpetas/23.01 I_Carpeta.sql
+++ 20. ProcedimientosAlmacenados/22. Carpetas/23.01 I_Carpeta.sql
@@ -10,12 +10,18 @@ CREATE PROCEDURE I_Carpeta
     in p_nombre VARCHAR(45)
     )
 BEGIN
-	IF EXISTS(select *from Carpetas where nombre = p_nombre)
+	IF EXISTS(select * from Carpetas where nombre = p_nombre)
 		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ya existe una carpeta con el mismo nombre y misma posicion';
 	ELSEIF (p_nombre is null or p_nombre='')
 		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El nombre de la carpeta nos puede ser nulo o vacio';
-    ELSE
-		INSERT INTO Carpetas value(default,p_idUsuario,p_idCarpetaPadre,p_nombre,FALSE);
-    END IF;
+	ELSE
+		INSERT INTO Carpetas value(
+			default,
+			p_idUsuario,
+			p_idCarpetaPadre,
+			p_nombre,
+			FALSE
+		);
+	END IF;
 END //
 DELIMITER ;
