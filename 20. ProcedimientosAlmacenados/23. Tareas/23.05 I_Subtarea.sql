777f7cc79bef17f3e69d1a4409377a540ac200a1
Luis Guzman <guzman.barriga.luis@gmail.com>
2023-06-02 13:56:59 -0600

    insertar subtarea

---
+++ 20. ProcedimientosAlmacenados/23. Tareas/23.05 I_Subtarea.sql
@@ -0,0 +1,54 @@
+-- I_Subtarea
+-- Nueva subtarea
+
+SELECT curdate();
+
+select concat(curdate(),' 23:59:59');
+
+use bd_tareas;
+
+DROP PROCEDURE IF EXISTS I_Subtarea;
+DELIMITER //
+CREATE PROCEDURE I_Subtarea
+	(
+    in p_idUsuario int,
+    in p_idTareaPadre int,
+    in p_idCarpeta int,
+    in p_titulo varchar(45),
+    in p_descripcion varchar(200),
+    in p_fechaInicio datetime,
+    in p_fechaVencimiento datetime,
+    in p_prioridad ENUM('Alta', 'Media', 'Baja')
+    )
+BEGIN
+	IF( p_idCarpeta = NULL and p_fechaVencimiento = NULL)
+		THEN set p_fechaVencimiento = concat(curdate(),' 23:59:59');
+	END IF;
+	IF EXISTS(select * from tareas where titulo = p_titulo) 
+		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ya existe una carpeta con el mismo titulo y misma posicion';
+	ELSEIF (p_titulo is null or p_titulo='') 
+		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El titulo de la carpeta nos puede ser nulo o vacio';
+    ELSEIF(p_fechaInicio < now())
+		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La fecha de inicio es menor a la fecha en la que se creo la tarea';
+    ELSEIF(p_fechaInicio > p_fechaVencimiento)
+		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La fecha de vencimiento es menor a la fecha de inicio';
+    ELSEIF NOT EXISTS(select * from tareas where idTarea = p_idTareaPadre)
+		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'No existe la tarea padre';
+    ELSE
+		INSERT INTO tareas value
+        (
+        default,
+        p_idUsuario,
+        p_idTareaPadre,
+        p_idCarpeta,
+        p_titulo,
+        p_descripcion,
+        p_fechaInicio,
+        p_fechaVencimiento,
+        p_prioridad,
+        'No completado',
+        false
+        );
+    END IF;
+END //
+DELIMITER ;
