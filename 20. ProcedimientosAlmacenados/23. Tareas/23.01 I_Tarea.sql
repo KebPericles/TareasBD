f5630487115286f4aa484f6ada3ceacbab67d1f7
KebPericles <45951033+KebPericles@users.noreply.github.com>
2023-06-01 19:45:34 -0600

    Update 23.01 I_Tarea.sql Correcciones menores

--- 20. ProcedimientosAlmacenados/23. Tareas/23.01 I_Tarea.sql
+++ 20. ProcedimientosAlmacenados/23. Tareas/23.01 I_Tarea.sql
@@ -1,12 +1,9 @@
 -- I_Tarea
 -- Nueva tarea
 
-use bd_tareas;
-
 DROP PROCEDURE IF EXISTS I_Tarea;
 DELIMITER //
-CREATE PROCEDURE I_Tarea
-	(
+CREATE PROCEDURE I_Tarea(
     in p_idUsuario int,
     in p_idCarpeta int,
     in p_titulo varchar(45),
@@ -14,18 +11,30 @@ CREATE PROCEDURE I_Tarea
     in p_fechaInicio datetime,
     in p_fechaVencimiento datetime,
     in p_prioridad ENUM('Alta', 'Media', 'Baja')
-    )
+)
 BEGIN
-	IF EXISTS(select *from tareas where titulo = p_titulo) 
+	IF EXISTS(select * from Tareas where titulo = p_titulo) 
 		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Ya existe una carpeta con el mismo titulo y misma posicion';
   	ELSEIF (p_titulo is null or p_titulo='') 
 		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El titulo de la carpeta nos puede ser nulo o vacio';
-    ELSEIF(p_fechaInicio < now())
+	ELSEIF(p_fechaInicio < now())
 		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La fecha de inicio es menor a la fecha en la que se creo la tarea';
-    ELSEIF(p_fechaInicio > p_fechaVencimiento)
+	ELSEIF(p_fechaInicio > p_fechaVencimiento)
 		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'La fecha de vencimiento es menor a la fecha de inicio';
-    ELSE
-		INSERT INTO tareas value(default,p_idUsuario,null,p_idCarpeta,p_titulo,p_descripcion,p_fechaInicio,p_fechaVencimiento,p_prioridad,'No completado',false);
-    END IF;
+	ELSE
+		INSERT INTO Tareas values (
+			default,
+			p_idUsuario,
+			null,
+			p_idCarpeta,
+			p_titulo,
+			p_descripcion,
+			p_fechaInicio,
+			p_fechaVencimiento,
+			p_prioridad,
+			'No completado',
+			false
+		);
+	END IF;
 END //
 DELIMITER ;
