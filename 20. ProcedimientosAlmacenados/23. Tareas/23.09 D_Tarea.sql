b4050f237856bbb9cc87512e12d58fb01aa168d7
Luis Guzman <guzman.barriga.luis@gmail.com>
2023-06-03 14:46:16 -0600

    Eliminar si papelera es true y si es falso mandar a papelera

--- 20. ProcedimientosAlmacenados/23. Tareas/23.09 D_Tarea.sql
+++ 20. ProcedimientosAlmacenados/23. Tareas/23.09 D_Tarea.sql
@@ -9,14 +9,16 @@ CREATE PROCEDURE D_Tarea(
 )
 BEGIN
     -- Verificar si la tarea existe
-    IF NOT EXISTS (SELECT * FROM tareas WHERE idTarea = p_idTarea) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Tarea inválida';
+    IF NOT EXISTS (SELECT * FROM tareas WHERE idTarea = p_idTarea)
+		THEN SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Tarea inválida';
+	-- Si la tarea no está en la papelera, borrarla y sus tareas hijas
     ELSE
-
-        DELETE FROM tareas WHERE idTareaPadre = p_idTarea;
-
-		DELETE FROM tareas WHERE idTarea = p_idTarea;
-
+		IF EXISTS (SELECT * FROM tareas WHERE idTarea = p_idTarea and enPapelera = false)
+			THEN CALL U_TareaAPapelera(p_idTarea);
+		ELSE
+			DELETE FROM tareas WHERE idTareaPadre = p_idTarea;
+			DELETE FROM tareas WHERE idTarea = p_idTarea;
+		END IF;
     END IF;
 END //
 
