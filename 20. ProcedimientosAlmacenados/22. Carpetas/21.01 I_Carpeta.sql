0f6fc5455a942c7cfc655d3e513706e6f198bfa6
LFGB <132220647+LFGB3689@users.noreply.github.com>
2023-06-05 15:02:51 -0600

    Update 21.01 I_Carpeta.sql

--- 20. ProcedimientosAlmacenados/22. Carpetas/21.01 I_Carpeta.sql
+++ 20. ProcedimientosAlmacenados/22. Carpetas/21.01 I_Carpeta.sql
@@ -17,11 +17,7 @@ BEGIN
     IF NOT EXISTS (SELECT * FROM usuarios WHERE idUsuario = p_idUsuario) THEN
       SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El ID de usuario no existe.';
     ELSE
-      IF p_idCarpetaPadre IS NOT NULL AND NOT EXISTS (SELECT * FROM carpetas WHERE idCarpeta = p_idCarpetaPadre) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El ID de carpeta padre no existe.';
-      ELSE
-        INSERT INTO carpetas VALUES (default, p_idUsuario, p_idCarpetaPadre, p_Nombre, p_EnPapelera);
-      END IF;
+      INSERT INTO carpetas VALUES (default, p_idUsuario, p_idCarpetaPadre, p_Nombre, p_EnPapelera);
     END IF;
   END IF;
 END //
