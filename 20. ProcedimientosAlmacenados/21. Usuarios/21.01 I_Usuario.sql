e849adf1237ebad79f075ea4c35736b81d13a7fb
Fernandogf021207 <fgarcesf1700@alumno.ipn.mx>
2023-06-04 23:49:12 -0600

    I_Usuario actualizado

--- 20. ProcedimientosAlmacenados/21. Usuarios/21.01 I_Usuario.sql
+++ 20. ProcedimientosAlmacenados/21. Usuarios/21.01 I_Usuario.sql
@@ -1,17 +1,25 @@
--- I_Usuario
--- Nuevo usuario
-DROP PROCEDURE IF EXISTS;
+-- Stored Procedure: I_Usuario
+DROP PROCEDURE IF EXISTS I_Usuario;
 DELIMITER //
-CREATE PROCEDURE I_Usuario
-	(
-    in p_correo VARCHAR(45),
-	p_contrasena VARCHAR(45),
-	p_nombre VARCHAR(45),
-	p_apellido VARCHAR(45)
-    )
+
+CREATE PROCEDURE I_Usuario(
+  IN p_correo VARCHAR(255),
+  IN p_contrasena VARCHAR(255),
+  IN p_nombre VARCHAR(255),
+  IN p_apellido VARCHAR(255)
+)
 BEGIN
-	IF NOT EXISTS(select *from Usuarios where correo = p_correo)
-		then INSERT INTO usuarios value(default,p_correo,p_contrasena,p_nombre,p_apellido);
-	END IF;
+  IF p_correo IS NULL OR p_contraseña IS NULL THEN
+    SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El correo y la contraseña son obligatorios.';
+  ELSE
+    IF EXISTS (SELECT idUsuario, correo, contrasena, nombre, apellido FROM usuarios WHERE correo = p_correo) THEN
+      SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'El correo electrónico ya está en uso.';
+    ELSE
+      INSERT INTO usuarios (correo, contrasena, nombre, apellido)
+      VALUES (p_correo, p_contrasena, p_nombre, p_apellido);
+    END IF;
+  END IF;
 END //
 DELIMITER ;
+
+-- call I_Usuario('correoprueba2@gmail.com', 'contraseña','javier','west');
